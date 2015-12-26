25634ea49bccaf1e53f8d254813b37d6d86074e2
jollaman999 <admin@jollaman999.com>
2015-12-26 18:58:07 +0900

    f2fs-tools: Fix build conflict with exfat

--- lib/libf2fs.c
+++ lib/libf2fs.c
@@ -38,7 +38,7 @@
 char *hasmntopt (const struct mntent *mnt, const char *opt) {
 	const size_t optlen = strlen (opt);
 	char *rest = mnt->mnt_opts, *p;
-	
+
 	while ((p = strstr (rest, opt)) != NULL) {
 		if ((p == rest || p[-1] == ',')
 			&& (p[optlen] == '\0' || p[optlen] == '=' || p[optlen] == ','))
@@ -117,7 +117,7 @@ static u_int16_t *wchar_to_utf16(u_int16_t *output, wchar_t wc, size_t outsize)
 	return output + 2;
 }
 
-int utf8_to_utf16(u_int16_t *output, const char *input, size_t outsize,
+int f2fs_utf8_to_utf16(u_int16_t *output, const char *input, size_t outsize,
 		size_t insize)
 {
 	const char *inp = input;
@@ -203,7 +203,7 @@ static char *wchar_to_utf8(char *output, wchar_t wc, size_t outsize)
 	return output;
 }
 
-int utf16_to_utf8(char *output, const u_int16_t *input, size_t outsize,
+int f2fs_utf16_to_utf8(char *output, const u_int16_t *input, size_t outsize,
 		size_t insize)
 {
 	const u_int16_t *inp = input;
